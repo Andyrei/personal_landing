394e55bca24ebcfa4cf1406be967fae011c2055d
Andyrei <radoacaandrei3@gmail.com>
2025-07-17 00:56:22 +0200

    ADD: new pages

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Projects() {
+  return (
+    <h1> cd projects </h1>
+  );
+}
---
+++ src/app/skills/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Skills() {
+  return (
+    <h1> cd skills </h1>
+  );
+}
